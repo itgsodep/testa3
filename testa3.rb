5787342dc97b403c238b2924a04704a0e29bc314
itgsodep <edvard.panes@it-gymnasiet.se>
2014-12-14 00:05:46 +0100

    Initial commit

---
+++ testa3.rb
@@ -0,0 +1,12 @@
+print "Enter a value?"
+var1 = gets
+print "Enter a value?"
+var2 = gets
+puts  (var1.to_i*var2.to_i)
+
+
+print "Enter a value?"
+var1 = gets
+print "Enter a value?"
+var2 = gets
+puts  (var1.to_i*var2.to_i)
---
+++ testa4.rb
@@ -0,0 +1,9 @@
+def multiply(a,b)
+  a * b
+end
+
+print 'Enter a value?'
+a = gets.to_i
+print 'Enter a value?'
+b = gets.to_i
+puts multiply(a,b)
